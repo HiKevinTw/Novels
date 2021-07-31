c63f2502e985396f79b35b64203177b63c03bfac
KevinChen <kieventw@gmail.com>
2021-07-31 16:09:01 +0800

    add log 2

---
+++ Novels.Log/ShowLogAppender.cs
@@ -0,0 +1,17 @@
+﻿namespace Novels.Log
+{
+
+	public delegate void ShowLogHandler(string level, string message);
+	public class ShowLogAppender
+	{
+		public static ShowLogHandler ShowLogHandler { private get; set; }
+		public static void LogMethod(string level, string message)
+		{
+			if (ShowLogHandler != null)
+			{
+				ShowLogHandler(level, message);
+			}
+		}
+	}
+
+}
